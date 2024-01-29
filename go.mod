d88c9fac000346431098ff48055181bb604ed4a1
jake-pauls <jakepauls99@gmail.com>
2024-01-28 20:21:10 -0500

    Adding godotenv to read .env files

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gorilla/websocket v1.5.1
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -8,6 +8,7 @@ import (
 	"github.com/20TB-ZipBomb/GGJ_Platform/internal/logger"
 	"github.com/20TB-ZipBomb/GGJ_Platform/internal/utils"
 	"github.com/20TB-ZipBomb/GGJ_Platform/pkg/network"
+	"github.com/joho/godotenv"
 )
 
 var env = flag.String("env", "dev", "server environment")
@@ -18,6 +19,10 @@ func Run() {
 
 	logger.Init()
 	defer logger.Sync()
+
+	if err := godotenv.Load(); err != nil {
+		logger.Fatal("Failed to load .env file")
+	}
 
 	addr := ":" + os.Getenv("PORT")
 	logger.Infof("%s server running on %s", utils.SanitizeEnvFlag(*env), addr)
